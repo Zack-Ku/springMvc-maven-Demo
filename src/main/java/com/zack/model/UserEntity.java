e0ddd0ac038a627d3c7cb165b45763ec3a2ed910
Zack <gza_scnu@163.com>
2017-11-03 15:39:59 +0800

    sql almost done

---
+++ src/main/java/com/zack/model/UserEntity.java
@@ -0,0 +1,99 @@
+package com.zack.model;
+
+import java.util.Collection;
+
+@javax.persistence.Entity
+@javax.persistence.Table(name = "user", schema = "springDEMO", catalog = "")
+public class UserEntity {
+    private int id;
+    private String nickname;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Collection<BlogEntity> blogsById;
+
+    @javax.persistence.Id
+    @javax.persistence.Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @javax.persistence.Basic
+    @javax.persistence.Column(name = "nickname", nullable = false, length = 45)
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @javax.persistence.Basic
+    @javax.persistence.Column(name = "password", nullable = false, length = 45)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @javax.persistence.Basic
+    @javax.persistence.Column(name = "first_name", nullable = false, length = 45)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @javax.persistence.Basic
+    @javax.persistence.Column(name = "last_name", nullable = false, length = 45)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (id != that.id) return false;
+        if (nickname != null ? !nickname.equals(that.nickname) : that.nickname != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (nickname != null ? nickname.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
+    }
+
+    @javax.persistence.OneToMany(cascade = {}, mappedBy = "userByUserId")
+    public Collection<BlogEntity> getBlogsById() {
+        return blogsById;
+    }
+
+    public void setBlogsById(Collection<BlogEntity> blogsById) {
+        this.blogsById = blogsById;
+    }
+}
